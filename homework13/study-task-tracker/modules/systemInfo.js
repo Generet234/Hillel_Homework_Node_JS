2bd112cc8716545a8a7c39c909fc7260798c1e8f
Generet234 <119113290+Generet234@users.noreply.github.com>
2026-07-09 17:52:11 +0300

    Delete homework13/study-task-tracker directory

--- homework13/study-task-tracker/modules/systemInfo.js
+++
@@ -1,8 +0,0 @@
-const os = require("os")
-function systemInfo () {
-    console.log('Operation system:', os.platform());
-    console.log("Free memory", os.freemem())
-    console.log("Working time", os.uptime())
-    console.log("Quantity of CPU", os.cpus())
-}
-module.exports = {systemInfo}
