80a4ad42e5d8bc00f912e853f1fcc3e194e2dff6
Generet234 <119113290+Generet234@users.noreply.github.com>
2026-07-19 11:56:47 +0300

    Homework 14

---
+++ homework14/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the main page");
+})
+
+app.get("/about", (req, res) => {
+    res.send("This is the ABOUT page");
+})
+
+app.get("/time", (req, res) => {
+    const time = new Date().toLocaleTimeString('uk-UA')
+    res.send(`The time right now is ${time}`);
+})
+
+app.get("/error", (req, res) => {
+    res.status(500).send("Server Error");
+})
+
+app.get("/user/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`User with ID: ${id}`);
+})
+
+app.get("/search", (req, res) => {
+    const query = req.query.q;
+    if (query) res.send(`You are looking for: ${query}`);
+    else {
+        res.send(`You do not mention search request`);
+        res.status(400).send("Bad request");
+    }
+})
+
+app.get("/user/:id/orders", (req, res) => {
+    const status = req.query.status;
+    const id = req.params.id;
+    if (status && id) res.send(`User order ${id} with status ${status}`);
+    else {
+        res.send(`You do not mention id/orders in your search request`);
+        res.status(404).send("Id or order do not Found");
+    }
+})
+
+app.use((req, res) => {
+    res.status(404).send("Not Found")
+})
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+})
