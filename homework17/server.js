1e876b491ec8a88ec4e23033711d5566f6bc0ada
Generet234 <119113290+Generet234@users.noreply.github.com>
2026-07-23 22:15:29 +0300

    Homework 17

---
+++ homework17/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors')
+const connectDB = require('./config/db')
+const productRoutes = require('./routes/productRoutes');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.use('/api/products', productRoutes);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
